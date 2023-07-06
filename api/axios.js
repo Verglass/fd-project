38fd812582d94483e7789a5c3f739d18bed29542
Verglass <85168081+Verglass@users.noreply.github.com>
2023-07-06 23:14:14 +0200

    Update axios.js

--- api/axios.js
+++ api/axios.js
@@ -2,5 +2,6 @@ import axios from 'axios'
 
 export default axios.create({
     // baseURL: 'https://shopapi-production-7591.up.railway.app'
-    baseURL: 'http://localhost:3500'
+    baseURL: 'https://shopapi-dimonidias.b4a.run/'
+    // baseURL: 'http://localhost:3500'
 })
